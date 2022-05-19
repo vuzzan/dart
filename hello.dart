019b5ab5c7551250f45fd1ba16518910ce5cdb84
Neo Nghia <vuzzan@gmail.com>
2022-05-19 20:20:21 +0700

    first commit

---
+++ hello.dart
@@ -0,0 +1,17 @@
+main() {
+  // Hàm function chính của chương trình
+  print("Chao ban, day la chuong trinh dau tien");
+
+  // Dong lenh nay ko chay
+  // phep tinh +
+  // variable
+  var a = 5;
+  var b = 6;
+  var c = a + b;
+  print("Phep + " + c.toString());
+
+  print("Phep x: " + (a * b).toString());
+
+  print("Phep /" + (a / b).toString());
+  // print :  in text ra man hinh. c = int. c.toString()
+}
