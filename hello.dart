ba6aeafd77334309ad0271720472244987044533
Neo Nghia <vuzzan@gmail.com>
2022-05-30 19:56:17 +0700

    Findmin

--- hello.dart
+++ hello.dart
@@ -45,20 +45,25 @@ main() {
     print("-3Trong cau lenh else");
   }
 
+  //print(cong(4, 5).toString());
   // 3. kieu du lieu tap hop
   //
-  var listInt = [1, 2, 3, 14, 5, 6, 17, 8, 9, 10, 11];
+  var listInt = [1, 2, 3, -14, 15, -60, 17, 8, 9, -10, 11];
+  //
   print(listInt);
   // tim so 5, trong dong listInt
-  var index = findValue(listInt, 5);
-  if (index == -1) {
-    print("ko tim thay");
-  } else {
-    print("Tim thay o vi tri: " + index.toString());
-  }
+  // var index = findValue(listInt, 5);
+  // if (index == -1) {
+  //   print("ko tim thay");
+  // } else {
+  //   print("Tim thay o vi tri: " + index.toString());
+  // }
   //var i = 0;
   // listInt.length: tong so phan tu trong listInt =11
   // i= 0: dieu kien dau -  i< 11: diem ket thuc -   // i++ ( i = i+1)
+
+  //function timMin
+  findMin(listInt);
 }
 
 // findValue: ten function
@@ -77,6 +82,50 @@ findValue(arr, a) {
   }
   print("findValue " + index.toString());
   return index;
+}
+
+softArray(arr) {
+  //[1, 2, 3, 14, 5, 6, 17, 8, -9, 10, 11];
+  // => array da duoc sap xep the o thu tu tang dan
+  // -9 1 2 3 4 ...
+  /**
+   1. Tim so nho nhat
+   2. Lay so nho nhat cho qua array ket qua
+   3. Xoa so nho nhat trong array input
+   */
+}
+// [1, 2, 3, 14, 5, 6, 17, 8, -9, 10, 11];
+// function findMin:tim gia tri nho nhat cua 1 array int
+// Input: array int
+// Output: index cua Gia tri min
+findMin(List<int> arr) {
+  var minValue = 10000000;
+  var indexMinValue = -1;
+  var i = 0;
+  // ==== CHUA LAM CHI HET
+  for (i = 0; i < arr.length; i++) {
+    // value tai index i
+    var value = arr[i];
+    // so sanh voi tung so trong array
+    print("-- Vi tri: " +
+        i.toString() +
+        " value=" +
+        value.toString() +
+        " minvalue=" +
+        minValue.toString());
+    if (value < minValue) {
+      //
+      minValue = value;
+      indexMinValue = i;
+      print("  " + i.toString() + "  thay doi min: " + minValue.toString());
+    }
+  }
+  // het for
+  print("Minvalue = " +
+      minValue.toString() +
+      " tai vi tri: " +
+      indexMinValue.toString());
+  return minValue;
 }
 
 // function + a b
